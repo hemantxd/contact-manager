5b6d9bf7ecb7176a75e3644ffb317c815cb02653
Hemant Bisht <bishthemant169@gmail.com>
2026-05-09 13:03:07 +0530

    navbar

---
+++ src/app/_components/Navbar.tsx
@@ -0,0 +1,39 @@
+import Link from 'next/link'
+import React from 'react'
+import LogoutButton from './LogoutButton'
+
+const Navbar = () => {
+  const session = true
+  return (
+    <nav className='bg-white shadow-sm'>
+
+      <div className='container mx-auto p-4 flex justify-between items-center'>
+        <Link href='/' className='font-bold text-lg text-blue-600'>
+        Contact Manager
+        </Link>
+        <div className='flex gap-4'>
+          {session ? (
+            <>  
+            <Link href='/contact' className='hover:text-blue-600 mr-8'>
+              Contacts
+            </Link>
+            <LogoutButton />
+            </>
+          ):(
+            <>  
+            <Link href='/login' className='hover:text-blue-600 mr-5'>
+              Login
+            </Link>
+            <Link href='/register' className='hover:text-blue-600 mr-8'>
+              Register            
+            </Link> 
+            </>
+          )}
+        </div>
+
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
